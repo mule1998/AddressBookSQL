13c05650277b3c20a0cd5b457c7300e068fc1939
Shubham Mule <shubhambmule@gmail.com>
2021-11-12 10:21:23 +0530

    Added : Parameters in AddressBook

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,3 +3,19 @@
 ----------UC1--Create Addressbook Database------------ 
 CREATE DATABASE AddressBookService;
 USE AddressBookService;
+
+
+-------------UC2-------------
+------Create table------------------
+
+CREATE TABLE AddressBookTable(
+FirstName varchar(100) not null,
+LastName varchar(50),
+Address varchar(200),
+City varchar(150),
+StateName varchar(100),
+ZipCode int,
+Phonenum bigint,
+EmailId varchar(100)
+);
+SELECT * FROM AddressBookTable;
